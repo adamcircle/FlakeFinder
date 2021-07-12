d7621c841244220626a9c037b46c22e57e789968
Adam Circle <adamcskier@gmail.com>
2021-07-12 16:29:55 -0400

    - Frontend spinning up

--- FlakeFinder/settings.py
+++ FlakeFinder/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from FlakeFinder.secrets import *
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -110,4 +111,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, './static/'),
+)
